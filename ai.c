840da4c58a96774494da5cb1f5794ea66b1e6bce
BarkingTiger <kkhau@ucsc.edu>
2023-11-09 23:38:14 -0800

    Update ai.c

--- ai.c
+++ ai.c
@@ -1,3 +1,5 @@
+#include <inttypes.h>
+#include <stdio.h>
 #include <stdint.h>
 #include <stdbool.h>
 
@@ -5,7 +7,7 @@
 #define WIDTH 7
 
 static uint64_t top_mask(int col) {
-	return ((uint64_t)(1) << (HEIGHT - 1)) << col * (HEIGHT + 1);
+        return ((uint64_t)(1) << (HEIGHT - 1)) << col * (HEIGHT + 1);
 }
 
 static uint64_t bottom_mask(int col) {
@@ -13,7 +15,7 @@ static uint64_t bottom_mask(int col) {
 }
 
 static uint64_t column_mask(int col) {
-	return (((uint64_t)(1) << HEIGHT) - 1) << col * (HEIGHT + 1);
+        return (((uint64_t)(1) << HEIGHT) - 1) << col * (HEIGHT + 1);
 }
 
 bool canPlay(uint64_t mask, int col) {
@@ -22,91 +24,118 @@ bool canPlay(uint64_t mask, int col) {
 
 
 void play(uint64_t *current_position, uint64_t *mask, int col) {
-	*current_position ^= *mask;
-	*mask |= *mask + bottom_mask(col);
-	//moves += 1;
+        *current_position ^= *mask;
+        *mask |= *mask + bottom_mask(col);
+        //moves += 1;
 }
 
 static bool alignment(uint64_t pos) {
-	//horizontal
-	uint64_t m = pos & (pos >> (HEIGHT + 1));
-	if (m & (m >> (2 * (HEIGHT + 1)))) {
-		return true;
-	}
+        //horizontal
+        uint64_t m = pos & (pos >> (HEIGHT + 1));
+        if (m & (m >> (2 * (HEIGHT + 1)))) {
+                return true;
+        }
 
-	//diagnonal 1
-	m = pos & (pos >> HEIGHT);
-	if (m & (m >> (2 * HEIGHT))) {
-		return true;
-	}
+        //diagnonal 1
+        m = pos & (pos >> HEIGHT);
+        if (m & (m >> (2 * HEIGHT))) {
+                return true;
+        }
 
-	//diagonal 2
-	m = pos & (pos << (HEIGHT + 2));
-	if (m & (m >> (2 * (HEIGHT + 2)))) {
-		return true;
-	}
+        //diagonal 2
+        m = pos & (pos << (HEIGHT + 2));
+        if (m & (m >> (2 * (HEIGHT + 2)))) {
+                return true;
+        }
 
-	//vertical
-	m = pos & (pos >> 1);
-	if (m & (m >> 2)) {
-		return true;
-	}
+        //vertical
+        m = pos & (pos >> 1);
+        if (m & (m >> 2)) {
+                return true;
+        }
 
-	return false;
+        return false;
 }
 
 bool isWinningMove(uint64_t current_position, uint64_t mask, int col) {
-	uint64_t pos = current_position;
-	pos |= (mask & bottom_mask(col)) & column_mask(col);
-	return alignment(pos);
+        uint64_t pos = current_position;
+        pos |= (mask & bottom_mask(col)) & column_mask(col);
+        return alignment(pos);
 }
 
 int negamax(uint64_t current_position, uint64_t mask, int moves, int alpha, int beta) {
-	if (moves == WIDTH * HEIGHT) {
-	return 0;
-	}
-	
-	for (int x = 0; x < WIDTH; x += 1) {
-		if (canPlay(mask, x) && isWinningMove(current_position, mask, x)) {
-			return (WIDTH * HEIGHT + 1 - moves) / 2;
-		}
-	}
+        if (moves == WIDTH * HEIGHT) {
+                printf("END 1\n");
+                return 0;
+        }
 
-	int max = (WIDTH * HEIGHT - 1 - moves) / 2;
-	
-	if (beta > max) {
-		beta = max;
-		if (alpha >= beta) {
-			return beta;
-		}
-	}
+        for (int x = 0; x < WIDTH; x += 1) {
+                if (canPlay(mask, x) && isWinningMove(current_position, mask, x)) {
+                        printf("END 2\n");
+                        return (WIDTH * HEIGHT + 1 - moves) / 2;
+                }
+        }
 
-	for (int x = 0; x < WIDTH; x += 1) {
-		if (canPlay(mask, x)) {
-			//need to make position and mask pointers so they retain the play
-			uint64_t copy_position = current_position;
-			uint64_t copy_mask = mask;
+        int max = (WIDTH * HEIGHT - 1 - moves) / 2;
 
-			play(&copy_position, &copy_mask, x);
+        if (beta > max) {
+                beta = max;
+                if (alpha >= beta) {
+                        printf("END 3\n");
+                        return beta;
+                }
+        }
 
-			int score = -negamax(copy_position, copy_mask, moves + 1, -beta, -alpha);
+        for (int x = 0; x < WIDTH; x += 1) {
+                if (canPlay(mask, x)) {
+                        //need to make position and mask pointers so they retain the play
+                        uint64_t copy_position = current_position;
+                        uint64_t copy_mask = mask;
 
-			if (score >= beta) {
-				return score;
-			}
-			if (score > alpha) {
-				alpha = score;
-			}
-		}
-	}
-	return alpha;
+                        play(&copy_position, &copy_mask, x);
+
+                        int score = -negamax(copy_position, copy_mask, moves + 1, -beta, -alpha);
+
+                        if (score >= beta) {
+                                printf("END 4\n");
+                                return score;
+                        }
+                        if (score > alpha) {
+                                alpha = score;
+                        }
+                }
+        }
+        return alpha;
 }
 
 int playOptimalMove(char board[6][7], char side) {
-	board[0][0] = 'r';
-	char test2 = side;
-	if (test2 == 'y') {
-		return 0;
-	}
-	return 1;
+        uint64_t position = 0;
+        uint64_t mask = 0;
+        int counter = 0;
+        int moves = 0;
+        for (int i = 0; i < 7; i += 1) {
+                for (int j = 5; j > -1; j -= 1) {
+                        if (board[j][i] == side) {
+                                position |= 1UL << counter;
+                        }
+                        else {
+                                position |= 0 << counter;
+                        }
+                        if (board[j][i] != '-') {
+                                mask |= 1UL << counter;
+                                moves += 1;
+                        }
+                        else {
+                                mask |= 0 << counter;
+                        }
+                        counter += 1;
+                }
+                counter += 1;
+        }
+        printf("POSITION %" PRIu64 "\n", position);
+        printf("MASK %" PRIu64 "\n", mask);
+
+        return negamax(position, mask, moves, -21, 21);
 }
+
+                                                                                                         
